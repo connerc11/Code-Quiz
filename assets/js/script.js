835d5f0b1145cd560449a5d529ba17b41bb997b7
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-07-12 14:01:36 -0400

    styling quiz intro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,13 @@
-var timerEl = document.getElementById('countdown');
-var startBtn = document.getElementById('start');
-var mainEl = document.getElementById('main');
-var quizcontainerEl = document.getElementById('quiz')
-var resultsEl = document.getElementById('results')
-var myQuestions = []
+
+
+var quizContainer = document.getElementById('quiz')
+var resultsContainer = document.getElementById('results')
+var submitButton = document.getElementById('submit')
 
 
 
-// var message =
-//   'Get ready to take a coding quiz! Hint it revolves around JavaScript!';
-//   var words = message.split(' ');
-
-//   // Timer that counts down from 5
-// function countdown() {
-//     var timeLeft = 5;
-  
-//     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//     var timeInterval = setInterval(function() {
-//       // As long as the `timeLeft` is greater than 1
-//       if (timeLeft > 1) {
-//         // Set the `textContent` of `timerEl` to show the remaining seconds
-//         timerEl.textContent = timeLeft + ' seconds remaining';
-//         // Decrement `timeLeft` by 1
-//         timeLeft--;
-//       } else if (timeLeft === 1) {
-//         // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//         timerEl.textContent = timeLeft + ' second remaining';
-//         timeLeft--;
-//       } else {
-//         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//         timerEl.textContent = '';
-//         // Use `clearInterval()` to stop the timer
-//         clearInterval(timeInterval);
-//         // Call the `displayMessage()` function
-//         displayMessage();
-//       }
-//     }, 2000);
-//   }
-  
-//   // Displays the message one word at a time
-//   function displayMessage() {
-//     var wordCount = 0;
-  
-//     // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
-//     var msgInterval = setInterval(function() {
-//       if (words[wordCount] === undefined) {
-//         clearInterval(msgInterval);
-//       } else {
-//         mainEl.textContent = words[wordCount];
-//         wordCount++;
-//       }
-//     }, 500);
-//   }
-  
-//   startBtn.onclick = countdown;
-  
  
-const myQuestions = [
+var myQuestions = [
     {
       question: "What does 3 equal signs mean?",
       answers: {
@@ -106,12 +57,5 @@ const myQuestions = [
         },
         correctAnswer: "b"
       }
+];
 
-    
-  ];
-  
-  
-
-
-
-  
