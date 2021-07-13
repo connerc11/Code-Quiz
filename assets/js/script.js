80c961029056e11b8412954952a2c5794bcaf94f
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-07-13 12:39:13 -0400

    updating CSS class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,72 @@
 
+var startBtn = document.querySelector(".start8btn button");
+var infoBox = document.querySelector(".info8box");
+var exitBtn = info8box.querySelector(".exit8btn .leave");
+var continueBtn =info8box.querySelector(".exit8btn .restart");
 
-var quizContainer = document.getElementById('quiz')
-var resultsContainer = document.getElementById('results')
-var submitButton = document.getElementById('submit')
+
+
+startBtn.onclick = ()=>{
+    info8box.classList.add("activeInfo")
+}
 
 
 
  
-var myQuestions = [
-    {
-      question: "What does 3 equal signs mean?",
-      answers: {
-        a: "Close to",
-        b: "Approximately equal",
-        c: "Exactly equal"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which one of these is not a way to show a variable in JavaScript",
-      answers: {
-        a: "var",
-        b: "const",
-        c: "tog",
-        d: "let",
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which wording can you use to properly link JavaScript with HTML?",
-      answers: {
-        a: "Tight",
-        b: "Script",
-        c: "displayJS",
-        d: "RequireJS"
-      },
-      correctAnswer: "b"
-    },
-    {
-        question: "Which is the proper way of hiding code from being displayed?",
-        answers: {
-          a: "//",
-          b: "?0",
-          c: "?#",
-          d: "hideC",
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "Whic is the proper format for creating a JavaScript function?",
-        answers: {
-          a: "function the function:",
-          b: "function myFunction()",
-          c: "function hiFunction (",
-          d: "function myfunction )"
-        },
-        correctAnswer: "b"
-      }
-];
+
+
+
+
+
+// var myQuestions = [
+//     {
+//       question: "What does 3 equal signs mean?",
+//       answers: {
+//         a: "Close to",
+//         b: "Approximately equal",
+//         c: "Exactly equal"
+//       },
+//       correctAnswer: "c"
+//     },
+//     {
+//       question: "Which one of these is not a way to show a variable in JavaScript",
+//       answers: {
+//         a: "var",
+//         b: "const",
+//         c: "tog",
+//         d: "let",
+//       },
+//       correctAnswer: "c"
+//     },
+//     {
+//       question: "Which wording can you use to properly link JavaScript with HTML?",
+//       answers: {
+//         a: "Tight",
+//         b: "Script",
+//         c: "displayJS",
+//         d: "RequireJS"
+//       },
+//       correctAnswer: "b"
+//     },
+//     {
+//         question: "Which is the proper way of hiding code from being displayed?",
+//         answers: {
+//           a: "//",
+//           b: "?0",
+//           c: "?#",
+//           d: "hideC",
+//         },
+//         correctAnswer: "a"
+//       },
+//       {
+//         question: "Whic is the proper format for creating a JavaScript function?",
+//         answers: {
+//           a: "function the function:",
+//           b: "function myFunction()",
+//           c: "function hiFunction (",
+//           d: "function myfunction )"
+//         },
+//         correctAnswer: "b"
+//       }
+// ];
 
