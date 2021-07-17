8128724fe710314bd66cd53d8683ec16923d97cf
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-07-17 12:00:44 -0400

    working on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,12 @@ var info8box = document.querySelector(".info8box");
 var exit8btn = info8box.querySelector(".exit8btn .leave");
 var continue8btn = info8box.querySelector(".exit8btn .continue");
 var box8quiz = document.querySelector(".box8quiz");
+var results = document.querySelector(".results");
 var options8list = document.querySelector(".options8list");
-var timer8count = box8quiz.querySelector(".time .seconds");
+// var timer8count = box8quiz.querySelector(".timer .seconds");
+var timer8count = document.querySelector(".timer .seconds")
+var timer8questions = questions.length * 12;
+var timer8id 
 
 
 
@@ -14,6 +18,7 @@ var timer8count = box8quiz.querySelector(".time .seconds");
 //This will allow the quiz to added//
 start8btn.onclick = ()=>{
     info8box.classList.add("activeInfo");
+    timer8count.textContent = timer8questions;
 }
 
 //THis will take you back to the beginning//
@@ -24,19 +29,32 @@ exit8btn.onclick = ()=>{
 //This should prompt the quiz//
 continue8btn.onclick = ()=>{
     info8box.classList.remove("activeInfo");
-    box8quiz.classList.add("activeQuiz")
+    box8quiz.classList.add("activeQuiz");
     showQuestions(0);
     queCount(1);
-    startTimer(20);
+    // startTimer(20);
 }
 
 let display8count = 0;
 let que8numb = 1;
-let time8counter;
-let clock = 20;
+// let timer8questions;
+// let clock = 60;
+let totalScore = 0;
 
-const next8btn = box8quiz.querySelector(".next8btn")
 
+
+var restart8JavaScriptQ = results.querySelector(".button .redo");
+var quit8JavaScriptQ = results.querySelector(".button .end");
+
+
+
+
+quit8JavaScriptQ .onclick =()=>{
+    window.location.reload();
+}
+
+
+var next8btn = box8quiz.querySelector(".next8btn");
 //when next button is clicked
 next8btn.onclick = ()=>{
     if(display8count < questions.length - 1) {
@@ -46,8 +64,10 @@ next8btn.onclick = ()=>{
         queCount(que8numb);
         clearInterval(time8counter);
         startTimer(clock);
+        next8btn.style.display = "none";
     }else{
         console.log("Quiz has been finished");
+        showResultBox();
     }
 }
 
@@ -74,6 +94,8 @@ function optionSelected(answer){
     let corrAns = questions[display8count].answer;
     const allOptions = options8list.children.length;
     if(userAns === corrAns) {
+        totalScore += 1;
+        console.log(totalScore);
         answer.classList.add("correct");
         console.log("Answer is Right")
     }else{
@@ -90,21 +112,53 @@ function optionSelected(answer){
    for (i = 0; i < allOptions; i++) {
     options8list.children[i].classList.add("disabled")
    }
+next8btn.style.display = "block";
+
 }
 
-function startTimer(time){
-    time8counter = setInterval(timer, 1000);
-    function timer(){
-        timer8count.textContent = time;
-        time--;
-
-        if(time < 0){
-            clearInterval(counter);
-            time8counter.textContent = "00"
-        }
+function showResultBox() {
+    info8box.classList.remove("activeInfo");
+    box8quiz.classList.remove("activeQuiz")
+    results.classList.add("activeResult");
+    var finalText = results.querySelector(".score8text");
+    if(totalScore > 3) {
+        let scoreTag = '<span>Amazing Work! You got <p>'+ totalScore +'</p> out of<p>'+ questions.length +'</p></span>';
+        finalText.innerHTML = scoreTag;
+    }
+    else if(totalScore > 1) {
+        let scoreTag = '<span>Nice try, You got <p>'+ totalScore +'</p> out of<p>'+ questions.length +'</p></span>';
+        finalText.innerHTML = scoreTag;
+    }
+        else {
+            let scoreTag = '<span>Oops, You got <p>'+ totalScore +'</p> out of<p>'+ questions.length +'</p></span>';
+            finalText.innerHTML = scoreTag;
     }
 }
 
+
+
+    timer8id = setInterval(timer, 1000);
+function timer (){
+    question8counters --;
+    timer8count.textContent = timer8questions;
+
+    
+}
+
+//     function timer(){
+//         document.getElementById('timer').innerHTML=timer8counter;
+//   time--;
+//   if (count === 0){
+//     clearInterval(time8counter);
+//     document.getElementById('count').innerHTML='Done';
+//     console.log("You're out of time!");
+
+
+ 
+
+
+        
+        
 
 
 
